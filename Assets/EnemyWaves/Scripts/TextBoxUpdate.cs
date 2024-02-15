89c6e5763a371fecc776c2d7e7a6008caa2e1676
Alex <alex99valle@gmail.com>
2024-02-14 23:57:11 -0600

    made some changes on questionInterface2.0

--- Assets/EnemyWaves/Scripts/TextBoxUpdate.cs
+++ Assets/EnemyWaves/Scripts/TextBoxUpdate.cs
@@ -8,12 +8,14 @@ public class TextBoxUpdate : MonoBehaviour
     [SerializeField]
     private TMP_Text displayText;
     string[] lines;
+    int answer;
     public TextAsset lessonFile;
 
     // Start is called before the first frame update
     void Start()
     {
         lines = lessonFile.text.Split('\n'); // Split the text into lines
+        Debug.Log("first line is " + lines[0]);
     }
     public void DisplayRandomTrivia()
     {
@@ -22,18 +24,31 @@ public class TextBoxUpdate : MonoBehaviour
             int index = Random.Range(0, lines.Length); // Pick a random line
             Debug.Log("Index = " + index);
             int lmod = (index % 3); //Text file starts with a hint, the question, then it's answer, alternating
-            if (lmod == 1)
-            {
-                index++;
-            }
             if (lmod == 0)
             {
+                Debug.Log("lmod = " + lmod);
+                answer = index + 2;
+                index++;
+            }
+            else if (lmod == 2)
+            {
+                Debug.Log("lmod = " + lmod);
+                answer = index;
                 index--;
             }
+            else{
+                answer = index + 1;
+            }
 
+            Debug.Log("answer = " + lines[answer]);
+            Debug.Log("new Index = " + index);
             string trivia_bit = lines[index];
             // Display trivia/lesson on the screen using UI Text
             displayText.text = trivia_bit;
         }
+    }
+
+    public string getAnswer(){
+        return lines[answer];
     }
 }
---
+++ Assets/QuestionButton.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionButton : MonoBehaviour
+{
+    public GameObject press;
+    public GameObject questionInterface;
+    public TextBoxUpdate textBoxUpdate;
+    bool isPressed;
+    GameObject presser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (!isPressed){
+            press.transform.localPosition = new Vector3(0, 0.003f, 0);
+            presser = other.gameObject;
+            //onPress.Invoke();
+            isPressed = true;
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject == presser){
+            press.transform.localPosition = new Vector3(0, 0.015f, 0);
+            Debug.Log("gameObject.name is " + gameObject.name);
+            Debug.Log("in questionbutton answer = " + textBoxUpdate.getAnswer());
+            if (gameObject.name == textBoxUpdate.getAnswer()){
+                Debug.Log("Correct");
+            }
+            else{
+                Debug.Log("Incorrect");
+            }
+            isPressed = false;
+            questionInterface.SetActive(false);
+        }
+
+    }
+}
